8aa937c8cd8ce58ef2fbc43b8a74aed2ea019d12
EvgenCherkashyn <evgencherkashyn@gmail.com>
2016-04-28 12:53:38 +0300

    Init

---
+++ js/app.js
@@ -0,0 +1,80 @@
+ var app = angular.module('app',[]);
+
+ app.controller('menuList',function ($scope) {
+
+
+     $scope.foods=[
+
+         {
+             photo:"img/food1.png",
+             heading:'Название блюда короткое',
+             description:"crispy pork knuckle,mash,braised pearl onions,carrots.",
+             price:"560",
+             link:"food.html"
+
+         },
+         {
+             photo:"img/food2.png",
+             heading:"Название блюда длинное,в две строки или больше",
+             description:"crispy pork knuckle,mash,braised pearl onions,carrots.",
+             price:"560",
+             link:"food.html"
+
+         },
+         {
+             photo:"img/food3.png",
+             heading:"Название блюда длинное,в две строки или больше",
+             description:"crispy pork knuckle,mash,braised pearl onions,carrots.",
+             price:"560",
+             link:"food.html"
+
+         },   {
+             photo:"img/food4.png",
+             heading:"Название блюда длинное,в две строки или больше",
+             description:"crispy pork knuckle,mash,braised pearl onions,carrots.",
+             price:"560",
+             link:"food.html"
+
+         },
+         {
+             photo:"img/food1.png",
+             heading:'Название блюда короткое',
+             description:"crispy pork knuckle,mash,braised pearl onions,carrots.",
+             price:"560",
+             link:"food.html"
+
+         },
+         {
+             photo:"img/food2.png",
+             heading:"Название блюда длинное,в две строки или больше",
+             description:"crispy pork knuckle,mash,braised pearl onions,carrots.",
+             price:"560",
+             link:"food.html"
+
+         },
+         {
+             photo:"img/food3.png",
+             heading:"Название блюда длинное,в две строки или больше",
+             description:"crispy pork knuckle,mash,braised pearl onions,carrots.",
+             price:"560",
+             link:"food.html"
+
+         },   {
+             photo:"img/food4.png",
+             heading:"Название блюда длинное,в две строки или больше",
+             description:"crispy pork knuckle,mash,braised pearl onions,carrots.",
+             price:"560",
+             link:"food.html"
+
+         },
+
+
+
+     ];
+
+
+
+
+
+ });
+
